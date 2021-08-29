218e85a949b6a5f600b054b5b48a7aa67563a1f8
georgehao <haohongfan@gmail.com>
2021-08-29 22:11:09 +0800

    feat: update

---
+++ find.go
@@ -0,0 +1,7 @@
+package gomodtestb
+
+import "github.com/georgehao/gomodtestc"
+
+func Find(str string, num int) string {
+	return gomodtestc.PrintStr(str, num)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/georgehao/gomodtestb
+
+go 1.16
+
+require github.com/georgehao/gomodtestc v1.0.1 // indirect
